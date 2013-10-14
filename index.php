be00ec53bf66d2bf6983365f20a680ae16b156b9
KatyAverill <katy@crynix.com>
2013-10-14 17:56:30 -0400

    Initial Commit
    Initial Commit of Market Research.

---
+++ index.php
@@ -0,0 +1,15 @@
+<link href="stylesheets/styles.css" rel="stylesheet" />
+<?php
+echo "<html><body><table>\n\n";
+$f = fopen("market_research.csv", "r");
+while (($line = fgetcsv($f)) !== false) {
+        echo "<tr>";
+        foreach ($line as $cell) {
+                echo "<td>" . htmlspecialchars($cell) . "</td>";
+        }
+        echo "<tr>\n";
+}
+fclose($f);
+echo "\n</table></body></html>";
+
+?>
